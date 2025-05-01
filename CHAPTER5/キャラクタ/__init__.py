d3d2a42732a8d1323e0c1a09d227dbe65705cfc2
kureha001 <member@kureha.info>
2025-05-02 00:27:33 +0900

    終了ロジックを修正/キャラクタの名称変更(ch6に合わせるため)

--- CHAPTER5/キャラクタ/__init__.py
+++ CHAPTER5/キャラクタ/__init__.py
@@ -2,6 +2,6 @@
 #┃技術評論社 ゲームで学ぶPython！ CHAPTER5
 #┃キャラクター・モジュール
 #┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-from .プレイヤー import classプレイヤー
-from .ザコキャラ import classザコキャラ
-from .ボスキャラ import classボスキャラ
+from .自機      import class自機
+from .敵機ザコ  import class敵機ザコ
+from .敵機ボス  import class敵機ボス
