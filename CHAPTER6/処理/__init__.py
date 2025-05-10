4d260a1d0fd60b1d8b016e137673111a4bf64c4e
kureha001 <member@kureha.info>
2025-05-11 07:12:18 +0900

    背景のモジュール構成を最新化した(すべてのクラスが完了)

--- CHAPTER6/処理/__init__.py
+++ CHAPTER6/処理/__init__.py
@@ -2,5 +2,5 @@
 #┃技術評論社 ゲームで学ぶPython！ CHAPTER6:MAGA WING
 #┃処理モジュール
 #┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-from ._定数         import class所有者ID
-from .ゲーム        import classGame
+from .ID      import class所有者ID
+from .ゲーム  import classGame
