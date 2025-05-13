06ece235f53a951680c105e9ef261f70dbe9c484
kureha001 <member@kureha.info>
2025-05-13 13:07:15 +0900

    ファイルが壊れたので復旧

--- CHAPTER6/キャラクタ/__init__.py
+++ CHAPTER6/キャラクタ/__init__.py
@@ -2,10 +2,8 @@
 #┃技術評論社 ゲームで学ぶPython！ CHAPTER6:MAGA WING
 #┃キャラクター・モジュール
 #┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-#from .自機      import class自機
 from . import 自機
 from . import 敵機
 from . import アイテム
-
 from .弾    import *
 from .爆発  import * 
