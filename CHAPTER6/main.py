26d909ec51a7eb80e0f9e2221b2aa598b997c321
kureha001 <member@kureha.info>
2025-05-16 23:25:22 +0900

    開始時のシールドをMAX

--- CHAPTER6/main.py
+++ CHAPTER6/main.py
@@ -1,5 +1,14 @@
 #┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 #┃技術評論社 ゲームで学ぶPython！ CHAPTER6:MAGA WING
+#┃
+#┃・パッケージファイル化：
+#┃ pyxel package ./ main.py
+#┃
+#┃・HTML化：
+#┃ pyxel app2html CHAPTER6.pyxapp
+#┃
+#┃・リソースエディタ起動：
+#┃ pyxel edit ".\リソース\改造版.pyxres"                
 #┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 from main import ゲーム開始
 ゲーム開始()
