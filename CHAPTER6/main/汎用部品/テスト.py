6547741c01f1c3e44ca92c60e37302b64725ba5c
kureha001 <member@kureha.info>
2025-06-17 16:49:45 +0900

    DFPLay対応中

--- CHAPTER6/main/汎用部品/テスト.py
+++ CHAPTER6/main/汎用部品/テスト.py
@@ -1,5 +1,5 @@
 #====================================================== 
-# ＭＭＰライブラリ Ver 0.02 ペーター用 サンプル
+# ＭＭＰライブラリ Ver 0.03 Rottenmeier用 サンプル
 #------------------------------------------------------
 # Ver 0.02.005　2025/04/28 By Takanari.Kureha
 #       1.PWMエキスパンダ(PCA9685)マルチ対応
@@ -8,7 +8,7 @@
 #====================================================== 
 # インクルード
 #====================================================== 
-import mmpPeter
+import mmpRottenmeier
 import time
 
 
@@ -17,76 +17,57 @@ import time
 #====================================================== 
 #┬
 #〇MMPを実体化する。
-MMP = mmpPeter.mmp(
-    argMmpNum       = 2,                # 使用するHC4067の個数
-    argMmpAnaPins   = 3,                # 使用するHC4067のPin数
-    argMmpAdrPins   = (10,11,12,13),    # RP2040-Zero
-    #argMmpAdrPins   = (2,3,4,5),        # Arduino
-    argRundNum      = 10                # アナログ値の丸め
-    )
+MMP = mmpRottenmeier.mmp()
 #│
 #〇MMPを接続する。
-MMP.autoConnect()
+MMP.通信接続_自動()
 #│
 #◇┐MMPをテストする。
 #　├→（アナログ入力（繰返））
-mode = 0
-if mode == 0:
-    #〇繰り返しテスト（先頭と最終のチャンネルのみ表示）
-    MMP.analog_Test(
-        argLoop = 400,      # アドレス切替回数
-        argWait = 0.05,     # ウェイト(秒)
-        argAll  = True      # True:全件表示／False:先頭末尾のみ表示
-        )
+mode = 320
+if mode == 100:
+    繰返回数 = 1000    # アドレス切替回数
+    待時間   = 0.05  # ウェイト(秒)
+    全権表示 = True  # True:全件表示／False:先頭末尾のみ表示
 
+    print("--------------------")
+    print(" アナログ入力テスト")
+    print("--------------------")
+    print("(測定データ)")
+    MMP.アナログ設定(
+            2,  # 使用するHC4067の個数(1～4)
+            2, # 使用するHC4067のPin数(1～16)
+            100 # アナログ値の丸め(この数値以下は切り捨て)
+            )
+    time_start = time.time()
 
-#　├→（アナログ入力（1回））
-elif mode == 1:
-    print("アナログ入力")
-    for i in range(16):
-        #〇1回テスト（全チャンネル表示）
-        MMP.analog_IN_Each(i)
-        値 = ""
+    #◎└┐繰り返し読み取る。
+    for cntLoop in range(繰返回数):        
+        #◎└┐全アドレスから読み取る。
+        MMP.アナログ読取()
+        if 待時間 > 0 : time.sleep(待時間)
+        if 全権表示 : print("  %03i" % cntLoop,":", MMP.mmpAnaVal)
+        else        : print("  %03i" % cntLoop,":", MMP.mmpAnaVal[0],"～", MMP.mmpAnaVal[-1])
 
-        for j in range(4):
-            区切 = "" if j==0 else " , "
-            値 = f"{値}{区切}{str(MMP.mmpAnaVal[i][j]).zfill(4)}"
+    #◇結果を表示する。
+    time_end = time.time()
+    time_diff = time_end - time_start
 
-        print(f"{str(i).zfill(2)}ch：{値}")
-        time.sleep(0.1)
+    print("\n(実施条件)")
+    print("・繰返回数         : %i" % (繰返回数))
+    print("・アドレス変更回数 : %i" % (MMP.参加総人数 * 繰返回数))
 
-#　├→（ＰＷＭ：サーボモータ）
-elif mode == 2:
-    print("サーボ・モータ")
-    #〇チャンネル番号リスト(0～922, ････)
-    pwmNo = (0,1,2)
-
-    #〇動作リスト；(開始角度，終了角度，増分，待ち時間(秒))
-    pwmMove = (
-        (   0, 181,  30, 1.00 ),
-        ( 180,  -1, -15, 0.50 ),
-        (   0, 181,   1, 0.05 ),
-        ( 180,  91,  -1, 0.05 )
-    )
-
-    #◎└┐動作リストに従い繰り返す。
-    for move in pwmMove:
-        #◎└┐動作リストの内容に従い、増分しながら繰り返す。
-        for angle in range(move[0],move[1],move[2]):
-            #◎└┐チャンネル番号リストに従い、繰り返す。
-            for No in (pwmNo):
-                #○ＰＷＭ出力する。
-                MMP.PWM_ANGLE( No, angle )
-                #print(No, angle)
-            #○時間待ちする。
-            time.sleep(move[3])
+    print("\n(測定結果)")
+    cntTtl = 繰返回数 * MMP.スイッチ数 * MMP.参加総人数
+    print("・合計時間   : %02.06f秒"       % (time_diff             ))
+    print("・データ平均 : %01.06f秒\n"   % (time_diff/cntTtl        ))
 
 #　├→（ＰＷＤ：ＤＣモータ）
-elif mode ==3:
+elif mode ==200:
     print("ＰＷＤ：ＤＣモータ")
-    番号    = 0
+    番号    = 0     #恐竜ランドのDCモータ
     最小    = 1800
-    最大    = 3800 #4095:デューティー比100%
+    最大    = 3800  #4095がデューティー比100%で最大になる
     間隔S   = 30
     間隔E   = -50
     停止    = 0.2
@@ -105,30 +86,77 @@ elif mode ==3:
     MMP.PWM_VALUE( 番号, -1 )
 
 #　├→（ＰＷＤ：電力供給）
-elif mode == 4:
+elif mode == 210:
     print("ＰＷＤ：電力供給")
-    番号    = 1
-    最小    = 4095 #4095:デューティー比100%
+    番号    = 1     #恐竜ランドの音声モジュール＆LED
+    最小    = 4095  #4095:デューティー比100%
     停止    = 20
     for i in range(1):
         MMP.PWM_VALUE( 番号, 最小 )
         time.sleep(停止)
         MMP.PWM_VALUE( 番号, 0 )
 
-#　├→（デジタル出力）
-elif mode == -1:
-    print("デジタル出力")
-    light = True
-    for j in range(10):
-        light = not light
-        if( light ):
-            MMP.portOut_bit(2, 1)
-            print("ON")
-        else:
-            MMP.portOut_bit(2, 0)
-            print("OFF")
-        time.sleep(5)
+#　├→（DFPlayer）
+elif mode == 300:
+    print("ｍｐ３プレイヤー")
+
+    print("・ボリューム設定")
+    print(MMP.DFP_Volume(20))
+
+    print("・連続再生")
+    for track in [1, 2, 3]:
+        print(MMP.DFP_Play(track))
+        time.sleep(3)
+
+    print("・１曲目を再生")
+    print(MMP.DFP_Play(1))
+    time.sleep(5)
+
+    print("・一時停止")
+    print(MMP.DFP_Pause())
+    time.sleep(3)
+
+    print("・再開")
+    print(MMP.DFP_Resume())
+    time.sleep(5)
+
+    print("・停止")
+    print(MMP.DFP_Stop())
+
+#　├→（DFPlayer）
+elif mode == 310:
+    print("ｍｐ３プレイヤー")
+
+    機器番号 = 1 # 1 or 2
+
+    print("・機器情報")
+    print("1台目：",MMP.DFP_Info(1))
+    print("2台目：",MMP.DFP_Info(2))
+
+    print("・ボリューム設定")
+    print(MMP.DFP_Volume(機器番号,20))
+
+    print("・１曲目を再生")
+    print(MMP.DFP_Play(機器番号,1))
+    time.sleep(5)
+
+    print("・停止")
+    print(MMP.DFP_Stop(機器番号))
+
+#　├→（DFPlayer）
+elif mode == 320:
+    print("ｍｐ３プレイヤー")
+
+    機器番号 = 1 # 1 or 2
+
+    print("・１曲目を再生")
+#    print(MMP.DFP_Play(機器番号,1))
+    print(MMP.DFP_PlayFolderTrack(機器番号,3,1))
+    time.sleep(5)
+
+    print("・停止")
+    print(MMP.DFP_Stop(機器番号))
 #│
 #〇MMPを切断する。
-MMP.disconnect
+MMP.通信切断
 #┴
